f0c689ad1414b9cae7a1775ac4b8033acf2e33e5
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2016-09-12 00:29:53 -0700

    Demonstrate HMAC in Rust with test cases.

--- src/sodium.rs
+++ src/sodium.rs
@@ -1,5 +1,6 @@
 // thank you to rust docs and dnaq/sodiumoxide!
 use libc::{c_int, c_ulonglong, size_t};
+use std::mem;
 
 #[repr(C)]
 pub struct crypto_hash_sha256_state {
@@ -9,7 +10,7 @@ pub struct crypto_hash_sha256_state {
 }
 
 #[repr(C)]
-struct crypto_auth_hmacsha256_state {
+pub struct crypto_auth_hmacsha256_state {
     ictx: crypto_hash_sha256_state,
     octx: crypto_hash_sha256_state,
 }
@@ -25,7 +26,7 @@ pub struct crypto_hash_sha512_state {
 }
 
 #[repr(C)]
-struct crypto_auth_hmacsha512_state {
+pub struct crypto_auth_hmacsha512_state {
     ictx: crypto_hash_sha512_state,
     octx: crypto_hash_sha512_state,
 }
@@ -73,28 +74,28 @@ extern {
      * https://github.com/jedisct1/libsodium/blob/master/src/libsodium/crypto_auth/hmacsha512/cp/hmac_hmacsha512.c
      * https://github.com/jedisct1/libsodium/blob/master/src/libsodium/crypto_verify/64/ref/verify_64.c
      */
-    fn crypto_auth_hmacsha512_bytes() -> size_t;
+    pub fn crypto_auth_hmacsha512_bytes() -> size_t;
 
-    fn crypto_auth_hmacsha512_keybytes() -> size_t;
+    pub fn crypto_auth_hmacsha512_keybytes() -> size_t;
 
-    fn crypto_auth_hmacsha512_init(
+    pub fn crypto_auth_hmacsha512_init(
         state: *mut crypto_auth_hmacsha512_state,
         key: *const u8,
         keylen: size_t,
     ) -> c_int;
 
-    fn crypto_auth_hmacsha512_update(
+    pub fn crypto_auth_hmacsha512_update(
         state: *mut crypto_auth_hmacsha512_state,
         m: *const u8,
         mlen: c_ulonglong,
     ) -> c_int;
 
-    fn crypto_auth_hmacsha512_final(
+    pub fn crypto_auth_hmacsha512_final(
         state: *mut crypto_auth_hmacsha512_state,
         a: *mut [u8; crypto_auth_hmacsha512_BYTES],
     ) -> c_int;
 
-    fn crypto_verify_64(
+    pub fn crypto_verify_64(
         x: *const [u8; 64],
         y: *const [u8; 64],
     ) -> c_int;
@@ -102,6 +103,52 @@ extern {
 
 pub fn get_hmac_sha512_digest_size() -> usize {
     return unsafe { crypto_auth_hmacsha256_bytes() as usize };
+}
+
+#[test]
+fn test_hmac_sha256() {
+    unsafe {
+        let mut context: crypto_auth_hmacsha256_state = mem::uninitialized();
+        let key = b"";
+        let message = b"";
+        let mut output = [0; 32];
+
+        crypto_auth_hmacsha256_init(&mut context, key.as_ptr(), key.len());
+        crypto_auth_hmacsha256_update(&mut context, message.as_ptr(),
+            message.len() as c_ulonglong);
+        crypto_auth_hmacsha256_final(&mut context, &mut output);
+
+        let expected = [0xb6, 0x13, 0x67, 0x9a, 0x08, 0x14, 0xd9, 0xec, 0x77, 0x2f, 0x95, 0xd7,
+            0x78, 0xc3, 0x5f, 0xc5, 0xff, 0x16, 0x97, 0xc4, 0x93, 0x71, 0x56, 0x53, 0xc6, 0xc7,
+            0x12, 0x14, 0x42, 0x92, 0xc5, 0xad];
+
+        assert_eq!(expected, output);
+        assert_eq!(0, crypto_verify_32(&expected, &output));
+    }
+}
+
+#[test]
+fn test_hmac_sha512() {
+    unsafe {
+        let mut context: crypto_auth_hmacsha512_state = mem::uninitialized();
+        let key = b"";
+        let message = b"";
+        let mut output = [0; 64];
+
+        crypto_auth_hmacsha512_init(&mut context, key.as_ptr(), key.len());
+        crypto_auth_hmacsha512_update(&mut context, message.as_ptr(),
+            message.len() as c_ulonglong);
+        crypto_auth_hmacsha512_final(&mut context, &mut output);
+
+        let expected = [0xb9, 0x36, 0xce, 0xe8, 0x6c, 0x9f, 0x87, 0xaa, 0x5d, 0x3c, 0x6f, 0x2e,
+            0x84, 0xcb, 0x5a, 0x42, 0x39, 0xa5, 0xfe, 0x50, 0x48, 0x0a, 0x6e, 0xc6, 0x6b, 0x70,
+            0xab, 0x5b, 0x1f, 0x4a, 0xc6, 0x73, 0x0c, 0x6c, 0x51, 0x54, 0x21, 0xb3, 0x27, 0xec,
+            0x1d, 0x69, 0x40, 0x2e, 0x53, 0xdf, 0xb4, 0x9a, 0xd7, 0x38, 0x1e, 0xb0, 0x67, 0xb3,
+            0x38, 0xfd, 0x7b, 0x0c, 0xb2, 0x22, 0x47, 0x22, 0x5d, 0x47];
+
+        assert_eq!(expected[..], output[..]);
+        assert_eq!(0, crypto_verify_64(&expected, &output));
+    }
 }
 
 #[test]
