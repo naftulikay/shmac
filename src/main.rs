fd4cdfdb816f90ad9313dc70d29c3d6a18e148da
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2016-09-26 21:21:05 -0700

    Successfully linking now and predictable.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,5 @@
 extern crate shmac;
 
-use shmac::sodium::kdf::{safe_argon2i_hash_raw_32, Argon2ErrorCodes};
-
 fn main() {
-    // println!("Hello, World! {:?}", [1, 255, 127].iter().map(|x| { format!("{:#x}", x) }).collect::<Vec<String>>());
-
-    let passphrase = "passphrase";
-    let salt = [0; 32];
-
-    safe_argon2i_hash_raw_32(3, 12, 1, passphrase.as_bytes(), &salt).and_then(|n| {
-        Ok(n)
-    });
-
-    // generate().and_then(|result| {
-    //     println!("thing");
-    //     Ok(result)
-    // });
+    println!("Hello, World! {:?}", [1, 255, 127].iter().map(|x| { format!("{:#x}", x) }).collect::<Vec<String>>());
 }
--- src/sodium/kdf.rs
+++ src/sodium/kdf.rs
@@ -2,82 +2,37 @@
  * libsodium KDF bindings.
  */
 
-use libc::{c_int, size_t};
-
-
-#[repr(C)]
-pub enum Argon2ErrorCodes {
-    OK                      = 0,
-    OutputPointerNull       = -1,
-    OutputTooShort          = -2,
-    OutputTooLong           = -3,
-    PasswordTooShort        = -4,
-    PasswordTooLong         = -5,
-    SaltTooShort            = -6,
-    SaltTooLong             = -7,
-    ADTooShort              = -8,
-    ADTooLong               = -9,
-    SecretTooShort          = -10,
-    SecretTooLong           = -11,
-    TooLittleTime           = -12,
-    TooMuchTime             = -13,
-    TooLittleMemory         = -14,
-    TooMuchMemory           = -15,
-    TooFewLanes             = -16,
-    TooManyLanes            = -17,
-    PasswordPointerMismatch = -18,
-    SaltPointerMismatch     = -19,
-    SecretPointerMismatch   = -20,
-    ADPointerMismatch       = -21,
-    MemoryAlocationError    = -22,
-    FreeMemoryCBKNull       = -23,
-    AllocateMemoryCBKNull   = -24,
-    IncorrectParameter      = -25,
-    IncorrectType           = -26,
-    OutputPointerMismatch   = -27,
-    TooFewThreads           = -28,
-    TooManyThreads          = -29,
-    MissingArgs             = -30,
-    EncodingFail            = -31,
-    DecodingFail            = -32,
-    ThreadFail              = -33,
-    DecodingLengthFail      = -34,
-    VerifyMismatch          = -35,
-}
+use libc::{c_int, c_ulonglong, size_t};
 
 #[link(name = "sodium")]
 extern {
-    /*
-    int argon2i_hash_raw(const uint32_t t_cost, const uint32_t m_cost,
-                     const uint32_t parallelism, const void *pwd,
-                     const size_t pwdlen, const void *salt,
-                     const size_t saltlen, void *hash, const size_t hashlen);
-    */
-    pub fn argon2i_hash_raw(
-        t_cost: u32,
-        m_cost: u32,
-        parallelism: u32,
-        pwd: *const u8,
-        pwdlen: size_t,
+
+    // int crypto_pwhash_argon2i(unsigned char * const out,
+    //                       unsigned long long outlen,
+    //                       const char * const passwd,
+    //                       unsigned long long passwdlen,
+    //                       const unsigned char * const salt,
+    //                       unsigned long long opslimit, size_t memlimit,
+    //                       int alg)
+
+    fn crypto_pwhash_argon2i(
+        out: *mut [u8],
+        outlen: c_ulonglong,
+        passwd: *const u8,
+        passwdlen: c_ulonglong,
         salt: *const u8,
-        saltlen: size_t,
-        hash: *mut [u8],
-        hashlen: size_t,
+        opslimit: c_ulonglong,
+        memlimit: size_t,
+        alg: c_int,
     ) -> c_int;
+
+    fn crypto_pwhash_argon2i_saltbytes() -> size_t;
 }
 
-pub fn safe_argon2i_hash_raw_32(t_cost: u32, m_cost: u32, parallelism: u32, pwd: &[u8],
-        salt: &[u8]) -> Result<[u8; 32], i32> {
+pub const CRYPTO_PWHASH_ARGON2I_SALTBYTES: usize = 16;
 
-    let mut output = [0; 32];
-
-    let rc = unsafe {
-        argon2i_hash_raw(t_cost, m_cost, parallelism, pwd.as_ptr(), pwd.len() as size_t,
-            salt.as_ptr(), salt.len() as size_t, &mut output, 32)
-    };
-
-    match rc {
-        0 => Ok(output),
-        _ => Err(rc),
-    }
+#[test]
+fn test_crypto_pwhash_argon2i_saltbytes() {
+    assert_eq!(CRYPTO_PWHASH_ARGON2I_SALTBYTES,
+        unsafe { crypto_pwhash_argon2i_saltbytes() })
 }
